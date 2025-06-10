acfb66f32ee32cc9b6bc6225dac456fc62b78793
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-06-10 15:03:15 +0530

    lect 3 completed

--- basicsreact/src/App.jsx
+++ basicsreact/src/App.jsx
@@ -1,10 +1,11 @@
-
+import Chai from "./chai"
 
 function App() {
 
   return (
     <>
-      <h1>Ripunjay Rai dd vvb</h1>
+      <Chai/>
+      <h1>hello</h1>
     </>
   )
 }
---
+++ basicsreact/src/chai.jsx
@@ -0,0 +1,6 @@
+function chai(){
+    return (
+        <h2>chai is ready</h2>
+    );
+}
+export default chai; 
