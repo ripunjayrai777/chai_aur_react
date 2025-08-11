716bf5044ecfa586997555ffc2ec394a7c9e3484
ripunjayrai777 <ripunjayrai777@gmail.com>
2025-08-11 17:51:58 +0530

    ....

---
+++ tailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-orange-500 p-5 rounded-xl text-white'>Tailwind Test</h1>
+    </>
+  )
+}
+
+export default App
